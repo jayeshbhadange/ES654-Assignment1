346554221ec358dc2f8a893e6d55630b76a4c8b9
jayeshbhadange <90170940+jayeshbhadange@users.noreply.github.com>
2023-01-20 11:59:37 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -13,34 +13,56 @@ def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     ensure that the function does not fail in corner cases.
     """
     assert y_hat.size == y.size
-    # TODO: Write here
-    pass
+    y_hat=y_hat.to_numpy()
+    y=y.to_numpy()
+    val=y_hat-y
+    cnt=np.count_nonzero(val== 0)
+    return float(cnt/len(y))
 
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the precision
     """
-    pass
+    assert y_hat.size == y.size
+    y_hat=y_hat.to_numpy()
+    y=y.to_numpy()
+    tp = np.sum((y == cls) & (y_hat == cls))
+    fp = np.sum((y != cls) & (y_hat == cls))
+    return float(tp / (tp + fp))
 
 
 def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the recall
     """
-    pass
+    assert y_hat.size == y.size
+    y_hat=y_hat.to_numpy()
+    y=y.to_numpy()
+    tp = np.sum((y == cls) & (y_hat == cls))
+    fn = np.sum((y = cls) & (y_hat != cls))
+    return float(tp / (tp + fn))
+    
 
 
 def rmse(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the root-mean-squared-error(rmse)
     """
-
-    pass
+    assert y_hat.size == y.size
+    y_hat=y_hat.to_numpy()
+    y=y.to_numpy()
+    err=(np.sqrt(np.mean((y_hat-y)**2)))
+    return float(err)
 
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the mean-absolute-error(mae)
     """
-    pass
+    assert y_hat.size == y.size
+    y_hat=y_hat.to_numpy()
+    y=y.to_numpy()
+    err=(np.mean(abs(y_hat-y)))
+    return err
+    
