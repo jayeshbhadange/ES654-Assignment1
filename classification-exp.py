b90fdb053b7559a798dc78da94d5d058dba2c537
sparsh-111 <89207720+sparsh-111@users.noreply.github.com>
2023-01-23 22:47:40 +0530

    Update classification-exp.py

--- classification-exp.py
+++ classification-exp.py
@@ -8,7 +8,7 @@ from sklearn.datasets import make_classification
 np.random.seed(42)
 X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=2, class_sep=0.5)
 
-#plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.scatter(X[:, 0], X[:, 1], c=y)
 
 X_train,y_train = X[:int(0.7*len(X))],y[:int(0.7*len(y))]
 X_test,y_test = X[int(0.7*len(X)):],y[int(0.7*len(y)):]
@@ -18,6 +18,7 @@ for criteria in ['information_gain','gini_index']:
     tree = DecisionTree(criterion = criteria,max_depth=4)
     tree.fit(pd.DataFrame(X_train),pd.Series(y_train))
     y_predict = tree.predict(pd.DataFrame(X_test))
+    print('\nDecision Tree\n')
     tree.plot()
     acc = accuracy(pd.Series(y_predict),pd.Series(y_test))
     print("Accuracy: ",acc)
